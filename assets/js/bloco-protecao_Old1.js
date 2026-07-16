01d019e8b731c5d4de62375f15a6734aebd0aa84
edusidegum <edusidegum@gmail.com>
2026-07-16 12:14:37 -0300

    Edicao para manutencao de diretorio

--- assets/js/bloco-protecao_Old1.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('contextmenu', e => e.preventDefault());
-document.addEventListener('copy', e => {
-    e.preventDefault();
-    e.clipboardData.setData('text/plain', 'Conteúdo Protegido - Ecossistema HLF @edusidegum. Cópia não autorizada.');
-});
